077fcbaf320178ccd37390bc2f241f67666e08a3
diegocam <camachojcd@gmail.com>
2015-07-22 23:42:48 -0400

    more updates

--- application/views/main_body.php
+++ application/views/main_body.php
@@ -1,16 +1,17 @@
 <div class="sidebar">
+    <h1 class="">Collections</h1>
     <ul>
-        <li><a href="">Type</a></li>
-        <li><a href="">Type</a></li>
-        <li><a href="">Type</a></li>
-        <li><a href="">Type</a></li>
-        <li><a href="">Type</a></li>
-        <li><a href="">Type</a></li>
-        <li><a href="">Type</a></li>
-        <li><a href="">Type</a></li>
+        <li><a href="/jovani-new-arrivals" title="New Arrivals">New<span> Arrivals</span></a></li>
+        <li><a href="/prom-dresses" title="Prom Dresses">Prom <span>Dresses</span></a></li>
+        <li><a href="/evening-dresses" title="Evening Dresses">Evening <span> Dresses</span></a></li>
+        <li><a href="/short-cocktail-dresses" title="Short Dresses">Short <span> Dresses</span></a></li>
+        <li><a href="/homecoming-dresses" title="Homecoming Dresses">Homecoming <span> Dresses</span></a></li>
+        <li><a href="/pageant-dresses" title="Pageant Dresses">Cocktail <span> Dresses</span></a></li>
+        <li><a href="/couture-dresses" title="Couture Dresses">Party <span> Dresses</span></a></li>
+        <li><a href="/wedding-dresses" title="Wedding Dresses">Wedding <span> Dresses</span></a></li>
     </ul>
 </div>
-<div class="slider">
-    <img src="some" alt="">
+<div class="center_images">
+    <img src="<?= base_url()?>assets/img/prom-dresses-2016.jpg" alt="">
 </div>
 <div class="clear_float"></div>
